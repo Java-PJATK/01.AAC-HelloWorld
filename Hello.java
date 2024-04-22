9cb33440b8ceed5912a1b05aa520eca9e90b85d0
piecucci <piecucci@gmail.com>
2024-04-22 16:21:39 +0200

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// AAC-HelloWorld/Hello.java
+ 
+/*
+ *  Program Hello
+ *  It prints "Hello, World"
+ */
+
+public class Hello { /* Entry class must be public
+                        File name = class name!   */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+    // signature of 'main' always like this
+}
